021ba27e244ce0a393ceec357852da0e9c868116
gen1usup <715702@gmail.com>
2022-09-01 17:39:58 +0300

    изменен test_first

---
+++ python2_2.py
@@ -0,0 +1,18 @@
+from selene.support.shared import browser
+from selene import be, have
+
+
+
+def test_first(open_browser):
+    assert browser.element("id='react-select-4-input']").matching(be.enabled) == False
+
+def test_second(open_browser):
+    browser.element('[id="firstName"]').type('selene')
+    browser.element('[id="lastName"]').type('selene1')
+    browser.element('[for="gender-radio-3"]').click()
+    browser.element('[id="userNumber"]').type('1234567890').press_enter()
+
+
+
+
+
